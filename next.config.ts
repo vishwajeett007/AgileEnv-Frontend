f4a2e7f748deebfa7be31ba8ce9e405b03afa351
vishwajeett007 <vishwajeetrajput282@gmail.com>
2025-12-14 18:06:05 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/api/auth/google/callback',
-  //       destination: '/callback?provider=google',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/api/auth/github/callback',
-  //       destination: '/callback?provider=github',
-  //       permanent: false,
-  //     }
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/api/auth/google/callback',
+        destination: '/callback?provider=google',
+        permanent: false,
+      },
+      {
+        source: '/api/auth/github/callback',
+        destination: '/callback?provider=github',
+        permanent: false,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
